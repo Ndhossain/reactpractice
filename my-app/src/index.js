e60ba393c3c38f3fe96f72fe4d0cb6c3f22cef39
Ndhossain <hhhssnahiid@gmail.com>
2022-02-11 14:22:01 +0600

    ilovereact

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,14 +1,40 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
 
 // eslint-disable-next-line no-unused-vars
-const name = 'World';
-const element = (
-    <h1 className="text">
-        <span>Hello {name}</span>
-        <img src="" alt="" />
-    </h1>
-);
 
-ReactDom.render(element, document.getElementById('root'));
+// const name = 'World';
+// class Clock extends React.PureComponent {
+//     render() {
+//         return (
+//             <h1 className="text">
+//                 <span>Hello {name}</span>
+//                 <br />
+//                 <span>The time is {new Date().toLocaleTimeString(this.props.locale)}</span>
+//             </h1>
+//         );
+//     }
+// }
+
+// const element = <Clock locale="bn-BD" />;
+
+// ReactDom.render(element, document.getElementById('root'));
+
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { date: new Date() };
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello, world!</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Clock />, document.getElementById('root'));
