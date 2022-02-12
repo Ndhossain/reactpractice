04fdd2deb34769d674a6b2070fc69a26f8dee544
Ndhossain <hhhssnahiid@gmail.com>
2022-02-12 15:16:36 +0600

    component & props

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,40 +1,23 @@
-// eslint-disable-next-line no-unused-vars
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDom from 'react-dom';
 
 // eslint-disable-next-line no-unused-vars
 
-// const name = 'World';
-// class Clock extends React.PureComponent {
-//     render() {
-//         return (
-//             <h1 className="text">
-//                 <span>Hello {name}</span>
-//                 <br />
-//                 <span>The time is {new Date().toLocaleTimeString(this.props.locale)}</span>
-//             </h1>
-//         );
-//     }
-// }
-
-// const element = <Clock locale="bn-BD" />;
-
-// ReactDom.render(element, document.getElementById('root'));
-
+const name = 'World';
+// eslint-disable-next-line react/prefer-stateless-function
 class Clock extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { date: new Date() };
-    }
-
     render() {
+        const proper = this.props;
         return (
-            <div>
-                <h1>Hello, world!</h1>
-                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-            </div>
+            <h1 className="text">
+                <span>Hello {name}</span>
+                <br />
+                <span>The time is {new Date().toLocaleTimeString(proper.locale)}</span>
+            </h1>
         );
     }
 }
 
-ReactDOM.render(<Clock />, document.getElementById('root'));
+const element = <Clock locale="bn-BD" />;
+
+ReactDom.render(element, document.getElementById('root'));
